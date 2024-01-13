c4e3d107dc714bd8c9b9a84a6a9908db31310479
Julia Chanas <juulia.chanas@gmail.com>
2024-01-13 17:30:46 +0100

    Modify navbar, add links - without styles

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,8 +1,26 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+// import styles from './NavBar.module.scss';
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <Navbar
+      bg='primary'
+      variant='dark'
+      expand='lg'
+      className='mt-4 mb-4 rounded'
+    >
+      <Navbar.Brand href='/'>Blog.app</Navbar.Brand>
+      <Nav className='me-auto'>
+        <Nav.Link as={NavLink} to='/'>
+          Home
+        </Nav.Link>
+        <Nav.Link as={NavLink} to='/about'>
+          About
+        </Nav.Link>
+      </Nav>
+    </Navbar>
   );
 };
 
