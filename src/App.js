b515ef618555ee0440ec84621e37bcc6d08c2820
Julia Chanas <juulia.chanas@gmail.com>
2024-01-13 14:43:19 +0100

    Create initialState, store and use provider in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-const App = () => {
-  return <h1>Hello world</h1>;
-};
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+const App = () => (
+  <Provider store={store}>
+    <h1>Hello world</h1>
+  </Provider>
+);
 
 export default App;
