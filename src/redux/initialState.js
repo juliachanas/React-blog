3b50e6c5f765108ebcb6db87fa4f6bd2aae6eec4
Julia Chanas <juulia.chanas@gmail.com>
2024-01-13 18:08:15 +0100

    Add data to InitialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,30 @@
 const initialState = {
-  posts: [],
+  posts: [
+    {
+      id: '1',
+      title: 'How to be happy',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe',
+    },
+    {
+      id: '2',
+      title: 'All about stress',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '04-05-2023',
+      author: 'Minni Mii',
+    },
+    {
+      id: '3',
+      title: 'I am strong',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '05-04-2024',
+      author: 'Eleonor Bee',
+    },
+  ],
 };
 
 export default initialState;
