2b89d1b6c72befcbbe6e018b23cb80cd8b866101
Julia Chanas <juulia.chanas@gmail.com>
2024-01-27 14:51:25 +0100

    Remove console.logs from dateToStr

--- src/utlis/dateToStr.js
+++ src/utlis/dateToStr.js
@@ -1,10 +1,7 @@
 export function dateToStr(date) {
   const month = date.getMonth() + 1;
-  console.log(month);
   const day = date.getDate();
-  console.log('Day', day);
   const year = date.getFullYear();
-  console.log('year', year);
 
   const formattedMonth = month < 10 ? '0' + month : month;
   const formattedDay = day < 10 ? '0' + day : day;
