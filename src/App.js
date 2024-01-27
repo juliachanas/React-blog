033747348ff81ccdd4f9c413cbd58ddbc8ae95f0
Julia Chanas <juulia.chanas@gmail.com>
2024-01-27 16:08:23 +0100

    Add categories path in App

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import CategoriesPage from './components/pages/CategoriesPage/CategoriesPage';
 
 const App = () => (
   <Provider store={store}>
@@ -19,6 +20,7 @@ const App = () => (
         <Header />
         <Routes>
           <Route path='/' element={<Homepage />} />
+          <Route path='/categories' element={<CategoriesPage />} />
           <Route path='/post/:id' element={<PostPage />} />
           <Route path='/add' element={<AddPostPage />} />
           <Route path='/edit/:id' element={<EditPostPage />} />
