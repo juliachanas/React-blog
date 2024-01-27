bde14e395570e8b64d85793e76183a8e19ea185d
Julia Chanas <juulia.chanas@gmail.com>
2024-01-27 14:23:07 +0100

    Add conditions for title, author and shortDescription

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -2,12 +2,20 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from 'react-hook-form';
 
 const PostForm = ({ action, actionText, ...props }) => {
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
   const [title, setTitle] = useState(props.title || '');
   const [author, setAuthor] = useState(props.author || '');
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
@@ -28,35 +36,40 @@ const PostForm = ({ action, actionText, ...props }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Row>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <label>Title</label>
-        </Col>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder='Add new title...'
-            type='text'
-            style={{ width: '50%' }}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <label>Author</label>
-        </Col>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <input
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            placeholder='Enter author...'
-            type='text'
-            style={{ width: '50%' }}
-          />
-        </Col>
-      </Row>
+    <form onSubmit={validate(handleSubmit)}>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          {...register('title', { required: true, minLength: 4 })}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type='text'
+          placeholder='Enter title'
+          style={{ width: '50%' }}
+        />
+        {errors.title && (
+          <small className='d-block form-text text-danger mt-2'>
+            This field is required
+          </small>
+        )}
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          {...register('author', { required: true, minLength: 4 })}
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          type='text'
+          placeholder='Enter title'
+          style={{ width: '50%' }}
+        />
+        {errors.author && (
+          <small className='d-block form-text text-danger mt-2'>
+            This field is required
+          </small>
+        )}
+      </Form.Group>
+
       <Row>
         <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
           <label>Published</label>
@@ -69,30 +82,24 @@ const PostForm = ({ action, actionText, ...props }) => {
             dateFormat='MM/dd/yyyy'
             style={{ width: '50%' }}
           />
+        </Col>
+      </Row>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Short description</Form.Label>
+        <Form.Control
+          {...register('shortDescription', { required: true, minLength: 21 })}
+          value={shortDescription}
+          onChange={(e) => setShortDescription(e.target.value)}
+          type='text'
+          placeholder='Enter short description'
+        />
+        {errors.shortDescription && (
+          <small className='d-block form-text text-danger mt-2'>
+            This field is required
+          </small>
+        )}
+      </Form.Group>
 
-          {/* <input
-            value={publishedDate}
-            onChange={(e) => setPublishedDate(e.target.value)}
-            placeholder='Enter date..'
-            type='text'
-            style={{ width: '50%' }}
-          /> */}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <label>Short description</label>
-        </Col>
-        <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
-          <ReactQuill
-            theme='snow'
-            value={shortDescription}
-            onChange={(shortDescription) =>
-              setShortDescription(shortDescription)
-            }
-          />
-        </Col>
-      </Row>
       <Row>
         <Col sm={12} style={{ marginBottom: '10px', marginTop: '10px' }}>
           <label>Main content</label>
